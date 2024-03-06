a388ff8471440cd0b5a93e1340e5726f61e51e46
yongveloper <aaa9015@naver.com>
2024-03-06 20:18:02 +0900

    feat: wiki 페이지 컴포넌트 생성

--- src/app/wiki/[id]/page.tsx
+++ src/app/wiki/[id]/page.tsx
@@ -1,5 +1,17 @@
+import BackButton from './_components/BackButton';
+import EditButton from './_components/EditButton';
+
 function Wiki() {
-  return <div>Wiki</div>;
+  return (
+    <div className="w-full">
+      <div className="flex justify-between">
+        <BackButton />
+        <EditButton />
+      </div>
+      <h1 className="text-3xl font-bold border-b border-gray-300 my-6">제목</h1>
+      <p>내용</p>
+    </div>
+  );
 }
 
 export default Wiki;
