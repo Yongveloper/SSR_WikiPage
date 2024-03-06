659477ae7794f328b13cf3a0221914d705a77bf2
yongveloper <aaa9015@naver.com>
2024-03-06 20:17:44 +0900

    feat: 수정 버튼 생성

---
+++ src/app/wiki/[id]/_components/EditButton.tsx
@@ -0,0 +1,12 @@
+function EditButton() {
+  return (
+    <button
+      type="button"
+      className="w-26 flex items-center justify-center px-2 py-2 text-sm text-blue-500 transition-colors duration-200 bg-white border rounded-lg gap-x-2 hover:bg-gray-100"
+    >
+      <span>수정</span>
+    </button>
+  );
+}
+
+export default EditButton;
