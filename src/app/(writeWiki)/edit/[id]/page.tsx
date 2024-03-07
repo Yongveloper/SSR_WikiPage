7f1bb9d4d05769d8c53475ff8c30431e213dc653
yongveloper <aaa9015@naver.com>
2024-03-07 23:35:08 +0900

    refactor: 커스텀훅 생성 후 적용

--- src/app/(writeWiki)/edit/[id]/page.tsx
+++ src/app/(writeWiki)/edit/[id]/page.tsx
@@ -2,11 +2,19 @@
 
 import { useParams } from 'next/navigation';
 import WikiForm from '@/app/(writeWiki)/_components/WikiForm';
+import { useGetPostDetailQuery } from '@/hooks/useGetPostDetailQuery';
 
 function Edit() {
   const { id } = useParams();
-
-  return <WikiForm isEditMode={true} id={id as string} />;
+  const { data, isLoading, isError } = useGetPostDetailQuery(id as string);
+  return (
+    <WikiForm
+      isEditMode={true}
+      id={id as string}
+      defaultTitle={data?.title}
+      defaultContent={data?.content}
+    />
+  );
 }
 
 export default Edit;
