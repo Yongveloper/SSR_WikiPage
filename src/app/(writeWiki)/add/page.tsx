c5a145624b28855de975071bce3f87f232d71409
yongveloper <aaa9015@naver.com>
2024-03-09 18:56:51 +0900

    feat: Add 페이지 metadata

--- src/app/(writeWiki)/add/page.tsx
+++ src/app/(writeWiki)/add/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import WikiForm from '../_components/WikiForm';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '코딩허브 | 게시물 작성',
+  description: '게시물 작성 페이지입니다.',
+};
 
 function Add() {
   return <WikiForm />;
