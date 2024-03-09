39d76f7d767a7fa4cd1c9a61f7ac0c98ec94c747
yongveloper <aaa9015@naver.com>
2024-03-09 18:53:40 +0900

    feat: Edit 페이지 metadata

--- src/app/(writeWiki)/edit/[id]/layout.tsx
+++ src/app/(writeWiki)/edit/[id]/layout.tsx
@@ -1,12 +1,23 @@
-import Spinner from '@/components/Spinner';
-import { Suspense } from 'react';
+import { Metadata } from 'next';
+import Header from './_components/Header';
+
+export const metadata: Metadata = {
+  title: '코딩허브 | 게시물 수정',
+  description: '게시물 수정 페이지',
+};
 
 interface IEditLayoutProps {
   children: React.ReactNode;
+  params: { id: string };
 }
 
-function EditLayout({ children }: IEditLayoutProps) {
-  return <Suspense fallback={<Spinner />}>{children}</Suspense>;
+function EditLayout({ children, params }: IEditLayoutProps) {
+  return (
+    <div className="w-full">
+      <Header id={params.id} />
+      {children}
+    </div>
+  );
 }
 
 export default EditLayout;
